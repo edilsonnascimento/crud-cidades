513e83eef0a2054a4b22aed47932cecf22d06578
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-04-14 09:01:31 -0300

    Paginas dinamicas adicionado Freemarker

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/controller/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/controller/CidadeController.java
@@ -1,13 +1,25 @@
 package br.edu.utfpr.cp.espjava.crudcidades.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar(){
-        return "crud.html";
+    public String listar(Model memoria){
+
+        var cidades = Set.of(
+          new Cidade("Cascavel", "PR"),
+          new Cidade("Porto Alegre", "RS"),
+          new Cidade("Florianopolis", "SC")
+        );
+
+        memoria.addAttribute("listaCidades", cidades);
+
+        return "crud";
     }
 }
